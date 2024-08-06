b3e6213ae18395771a70cead5a52e0c650422a26
Brayan_Feijó <78597094+Bfeijx12@users.noreply.github.com>
2024-08-06 15:41:45 -0300

    Add files via upload

---
+++ Sistema de Cadastro 01/src/Main.java
@@ -0,0 +1,15 @@
+import Tela.frmResponsavel;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       frmResponsavel telaResponsavel = new frmResponsavel();
+        telaResponsavel.setVisible(true);
+        
+    }
+    
+}
